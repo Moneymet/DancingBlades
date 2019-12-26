2b331900d07e1c776640fc3408b5e93bd9e70c60
Moneymet <christergum@hotmail.com>
2019-12-26 17:28:13 +0100

    Exported some functions out of handleKeyUp.

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import "./init";
 const weapon_search = document.getElementById("weapon_search");
 function handleKeyUp(event:any){
     const searched_name = (event.target.value as string).toLowerCase();
-    const searched_name_matched = searched_name.match(/"[^"]*"|\w+/g); //Matches Strings with " around them and words.
+    const searched_name_matched = matchQuoted(searched_name); 
     const weapon_rows = document.getElementsByClassName("weapon_rows");
     let searched_name_cleaned;
     if(searched_name_matched != null){
         searched_name_cleaned = searched_name_matched.map((word) =>{ //Removes " from search strings.
-            return word.replace(/"/g, "");
+            return cleanQuotes(word);
         })
     };
     for (let i = 0; i < weapon_rows.length; i++) {
@@ -22,19 +22,17 @@ function handleKeyUp(event:any){
             }
         }
         const weapon_search_result = weapon_name.search(searched_name);
-        if(weapon_found){
-            (weapon_rows[i] as HTMLInputElement).style.display = "";
-        } else{
-            (weapon_rows[i] as HTMLInputElement).style.display = "none";
-        }
+        (weapon_rows[i] as HTMLInputElement).style.display = weapon_found ? "" : "none";
     }
-    /*for (let item of weapon_rows) {
-        const weapon_name = item.children[0].innerHTML;
-        if(weapon_name.includes(searched_name)){
-            item.style.display = "";
-        } else{
-            item.style.display = "none";
-        }
-    }*/
+}
+
+//Matches Strings with " around them and words.
+function matchQuoted(matchString:string):string[]{
+    return matchString.match(/"[^"]*"|\w+/g);
+}
+
+//Removes " from search strings.
+function cleanQuotes(inputString:string):string{
+    return inputString.replace(/"/g, "");
 }
 weapon_search.onkeyup = handleKeyUp;
